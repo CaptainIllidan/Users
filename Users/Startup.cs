c823c986597c2311eaf5d57f342befe494944d3c
CaptainIllidan <37904051+CaptainIllidan@users.noreply.github.com>
2019-05-23 13:06:05 +0400

    https://github.com/CaptainIllidan/Users.git

--- Users/Startup.cs
+++ Users/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -26,9 +29,40 @@ namespace Users
         {
             services.AddTransient<IPasswordValidator<AppUser>,
                 CustomPasswordValidator>();
-
             services.AddTransient<IUserValidator<AppUser>,
             CustomUserValidator>();
+            services.AddSingleton<IClaimsTransformation,
+                LocationClaimsProvider>();
+            services.AddTransient<IAuthorizationHandler, BlockUsersHandler>();
+            services.AddTransient<IAuthorizationHandler, DocumentAuthorizationHandler>();
+
+            services.AddAuthorization(opts =>
+            {
+                opts.AddPolicy("DCUsers", policy =>
+                {
+                    policy.RequireRole("Users");
+                    policy.RequireClaim(ClaimTypes.StateOrProvince, "DC");
+                });
+                opts.AddPolicy("NotBob", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.AddRequirements(new BlockUsersRequirement("Bob"));
+                });
+                opts.AddPolicy("AuthorsAndEditors", policy =>
+                {
+                    policy.AddRequirements(new DocumentAuthorizationRequirement
+                    {
+                        AllowAuthors = true,
+                        AllowEditors = true
+                    });
+                });
+            });
+
+            services.AddAuthentication().AddGoogle(opts =>
+            {
+                opts.ClientId = "438374634170-282k3e373jc2usbpailfdsq9p1mr9nf8.apps.googleusercontent.com";
+                opts.ClientSecret = "UFnJXMeCyegZWHDb8kT4cI7f";
+            });
 
             services.AddDbContext<AppIdentityDbContext>(options =>
                 options.UseSqlServer(
@@ -58,8 +92,8 @@ namespace Users
             app.UseStaticFiles();
             app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
-            AppIdentityDbContext.CreateAdminAccount(app.ApplicationServices,
-                Configuration).Wait();
+            //AppIdentityDbContext.CreateAdminAccount(app.ApplicationServices,
+            //    Configuration).Wait();
         }
     }
 }
